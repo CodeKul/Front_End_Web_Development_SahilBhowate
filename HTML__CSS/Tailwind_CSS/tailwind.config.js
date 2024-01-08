8792445f3e76fa79679891ffeeb20b9a8c7ca2b1
Sahil Bhowate <sb202211@appystack.com>
2024-01-08 18:07:51 +0530

    Changes

--- HTML__CSS/Tailwind_CSS/tailwind.config.js
+++ HTML__CSS/Tailwind_CSS/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "624px",
+      // => @media (min-width: 624px) { ... }
+
+      laptop: "800px",
+      // => @media (min-width: 800px) { ... }
+    },
   },
   plugins: [],
-}
-
+};
