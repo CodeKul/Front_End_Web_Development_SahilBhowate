1f73b16517e5456fed3e14a4694e4dff2a9ad595
Sahil Bhowate <sb202211@appystack.com>
2024-01-23 10:26:24 +0530

    Changes

--- 11_AM_JS/Operators/script7.js
+++ 11_AM_JS/Operators/script7.js
@@ -1,7 +1,19 @@
-var a=10;
-var b=11;
+var a = 10;
+var b = 11;
 
-a++;  //a=a+1
-b--;  //b=b-1
+a++; //a=a+1
+b--; //b=b-1
 
-console.log("The value of a and b is ",a,b);
+//pre-increment
+// ++b;
+
+// post increment
+// b++;
+
+//pre-decrement
+// --b;
+
+//post-decrement
+// b--;
+
+console.log("The value of a and b is ", a, b);
