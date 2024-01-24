4d5ef50fd333d857f81bcb5805817dae1eac6eb5
Sahil Bhowate <sb202211@appystack.com>
2024-01-24 10:11:59 +0530

    Changes

--- 11_AM_JS/Flow Of Program/ifelseStatement.js
+++ 11_AM_JS/Flow Of Program/ifelseStatement.js
@@ -8,12 +8,3 @@ if (condition) {
 } else {
   //  block of code to be executed if the condition is false
 }
-
-//The elseif statement
-if (condition1) {
-  //  block of code to be executed if condition1 is true
-} else if (condition2) {
-  //  block of code to be executed if the condition1 is false and condition2 is true
-} else {
-  //  block of code to be executed if the condition1 is false and condition2 is false
-}
