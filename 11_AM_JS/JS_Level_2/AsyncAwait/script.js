f8c3c6eaa057d69ba0c4b0f504b3b11b5cd59212
Sahil Bhowate <sb202211@appystack.com>
2024-01-31 15:47:07 +0530

    Changes

---
+++ 11_AM_JS/JS_Level_2/AsyncAwait/script.js
@@ -0,0 +1,11 @@
+async function getData() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
