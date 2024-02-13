4c2016d5031663a0d92c8d6368d86ad513259749
Sahil Bhowate <sb202211@appystack.com>
2024-02-13 10:32:13 +0530

    Changes

---
+++ JS/JS_Level_2/4_Callback_Functions/script2.js
@@ -0,0 +1,8 @@
+setTimeout(startOperatingSystem, 5000);
+
+function startOperatingSystem() {
+  console.log("The operating system has been started");
+}
+
+console.log("Preparing Devices...");
+console.log("Loading Device Drivers...");
