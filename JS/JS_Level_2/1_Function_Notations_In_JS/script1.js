93f190e0ce969461f8e68ecf68497bf438a1f5bc
ak20221 <ak202221@appystack.com>
2024-02-14 18:16:48 +0530

    Changes

--- JS/JS_Level_2/1_Function_Notations_In_JS/script1.js
+++ JS/JS_Level_2/1_Function_Notations_In_JS/script1.js
@@ -1,6 +1,9 @@
 //we can assign a regular function to a variable
-let getData = function getDataOne() {
+let getInfo = function getDataOne() {
   console.log("The name is Tushar.");
 };
 
-getData();
+//function call
+getInfo();
+
+
